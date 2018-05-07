9ca3d803431ab644b161967f6189dd0ea20443f1
Mirela Peltekova <mpeltekova16@gmail.com>
2018-05-07 19:11:11 +0300

    user settings update

--- src/main/java/com/dominos/controller/HelloController.java
+++ src/main/java/com/dominos/controller/HelloController.java
@@ -15,6 +15,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dominos.model.address.Address;
 import com.dominos.model.address.AddressDao;
@@ -36,26 +37,26 @@ public class HelloController {
 			model.addAttribute("user", user);
 			return "index";
 		}
-		
-//		HttpSession session = request.getSession();
-		
-//		if (session.getAttribute("loggedUser")!=null) {
-//			session.invalidate();
-//		}
-		
+
+		// HttpSession session = request.getSession();
+
+		// if (session.getAttribute("loggedUser")!=null) {
+		// session.invalidate();
+		// }
+
 		return "indexLogged";
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String helloLogin(Model model) {
-		
+
 		User user = new User();
 		model.addAttribute("user", user);
 		return "index1";
 	}
 
 	@RequestMapping(value = "/index", method = RequestMethod.POST)
-	public String register( @ModelAttribute @Valid User user, BindingResult result,HttpSession s) {
+	public String register(@ModelAttribute @Valid User user, BindingResult result, HttpSession s) {
 		try {
 
 			if (result.hasFieldErrors()) {
@@ -72,7 +73,7 @@ public class HelloController {
 					return "index";
 				} else {
 					dao.register(user);
-					//HttpSession session = request.getSession();
+					// HttpSession session = request.getSession();
 					s.setMaxInactiveInterval(120);
 					s.setAttribute("loggedUser", user);
 					return "redirect:/drinks";
@@ -88,8 +89,7 @@ public class HelloController {
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
-	public String login(Model model, HttpSession s, @ModelAttribute @Valid User user,
-			BindingResult result) {
+	public String login(Model model, HttpSession s, @ModelAttribute @Valid User user, BindingResult result) {
 		try {
 
 			if (result.hasFieldErrors()) {
@@ -98,7 +98,7 @@ public class HelloController {
 			}
 			if (dao.existsUser(user.getEmail(), user.getPassword())) {
 				user = dao.getUser(user.getEmail());
-				//HttpSession session = request.getSession();
+				// HttpSession session = request.getSession();
 				s.setMaxInactiveInterval(12000);
 				s.setAttribute("loggedUser", user);
 				return "redirect:drinks";
@@ -111,61 +111,109 @@ public class HelloController {
 		result.addError(new ObjectError("err", "Invalid input"));
 		return "index1";
 	}
-	
+
 	// @RequestMapping(value = "/updateProfile", method = RequestMethod.GET)
-		//
-		// private String updateProfile(User user, HttpSession sess){
-		//
-		// sess.setAttribute("user", user);
-		//
-		// return "updateProfile";
-		// }
-		// @RequestMapping(value = "/updateProfile", method = RequestMethod.POST)
-		//
-		// private String doUpdateProfile(User user, HttpSession sess){
-		//
-		// user.setId(((User) sess.getAttribute("user")).getId());
-		// try {
-		// dao.updateUser(user);
-		// } catch (SQLException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// }
-		// try {
-		// user = dao.getUser(user.getEmail());
-		// } catch (ClassNotFoundException | SQLException e) {
-		// // TODO Auto-generated catch block
-		// e.printStackTrace();
-		// return "redirect:index.html";
-		//
-		// }
-		// sess.setAttribute("user", user);
-		//
-		// return "redirect:pizzas:";
-		// }
+	//
+	// private String updateProfile(User user, HttpSession sess){
+	//
+	// sess.setAttribute("user", user);
+	//
+	// return "updateProfile";
+	// }
+	// @RequestMapping(value = "/updateProfile", method = RequestMethod.POST)
+	//
+	// private String doUpdateProfile(User user, HttpSession sess){
+	//
+	// user.setId(((User) sess.getAttribute("user")).getId());
+	// try {
+	// dao.updateUser(user);
+	// } catch (SQLException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// }
+	// try {
+	// user = dao.getUser(user.getEmail());
+	// } catch (ClassNotFoundException | SQLException e) {
+	// // TODO Auto-generated catch block
+	// e.printStackTrace();
+	// return "redirect:index.html";
+	//
+	// }
+	// sess.setAttribute("user", user);
+	//
+	// return "redirect:pizzas:";
+	// }
 
-		@RequestMapping(value = "/updateProfile")
-		private String doUpdateProfile(Model model,User user,HttpSession sess) {
-			System.out.println(((User) sess.getAttribute("loggedUser")).getId());
-			user.setId(((User) sess.getAttribute("loggedUser")).getId());
-			try {
-				dao.updateUser(user);
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			try {
-				user = dao.getUser(user.getEmail());
-			} catch (ClassNotFoundException | SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-				return "redirect:index.html";
-
-			}
-			model.addAttribute("user",user);
-			sess.setAttribute("loggedUser", user);
-
-			return "updateProfile";
+	@RequestMapping(value = "/updateProfile", method = RequestMethod.POST)
+	private String doUpdateProfile(@ModelAttribute User registeredUser,HttpSession s,HttpServletRequest request,final RedirectAttributes redirectAttributes) {
+		if (s == null || s.getAttribute("loggedUser") == null) {
+			return "redirect:/index";
 		}
-	
+
+
+		System.out.println(((User) s.getAttribute("loggedUser")).getId());
+		
+		registeredUser.setId(((User) s.getAttribute("loggedUser")).getId());
+		
+		String fName=request.getParameter("firstname");
+		System.out.println(fName);
+		registeredUser.setFirstName(fName);
+		
+		String lName=request.getParameter("lastname");
+		System.out.println(lName);
+		registeredUser.setLastName(lName);
+		
+		String mail=request.getParameter("email");
+		System.out.println(mail);
+		registeredUser.setEmail(mail);
+
+		String pass1=request.getParameter("password");
+		String pass2=request.getParameter("password2");
+		
+		if(!pass1.equals(pass2)) {
+//			String message = "Invalid username or password!";
+//			 redirectAttributes.addFlashAttribute("error", message);
+			return "updateProfile";
+
+		}else {
+		//	request.setAttribute("equallPassword", null);
+
+		registeredUser.setPassword(pass1);
+		System.out.println(pass1);
+		try {
+			dao.updateUser(registeredUser);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return "error";
+
+			
+		}
+
+		s.setAttribute("loggedUser", registeredUser);
+
+		return "succesfullUpdate";
+		
+		}
+	}
+
+	@RequestMapping(value = "/updateProfile", method = RequestMethod.GET)
+	private String updateProfile(Model model, HttpSession s,HttpServletRequest request) {
+
+		if (s == null || s.getAttribute("loggedUser") == null) {
+			return "redirect:/index";
+		}
+
+		User u = new User();
+		User loggedUser = (User) s.getAttribute("loggedUser");
+		if (loggedUser != null) {
+			System.out.println("User exist");
+		} else {
+			System.out.println("User does not exist");
+		}
+		model.addAttribute("updatedUser", u);
+		request.setAttribute("equallPassword", null);
+
+		return "updateProfile";
+	}
+
 }
--- src/main/webapp/WEB-INF/views/jsp/updateProfile.jsp
+++ src/main/webapp/WEB-INF/views/jsp/updateProfile.jsp
@@ -1,8 +1,9 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
 	pageEncoding="ISO-8859-1"%>
 <jsp:include page="header.jsp"></jsp:include>
+<%@taglib prefix="sf" uri="http://www.springframework.org/tags/form"%>
 
-<%@ taglib prefix="f" uri="http://www.springframework.org/tags/form"%>
+<%@ taglib prefix='c' uri='http://java.sun.com/jsp/jstl/core'%>
 
 <style>
 .nameField {
@@ -10,41 +11,160 @@
 }
 
 .errorField {
-	color:  solid red;
+	color: solid red;
 	background-color: red;
 }
-</style>
-<div class="container">
-	<div class="info"></div>
-</div>
-<div class="form">
 
-	<div class="thumbnail">
-		<img src="img/small_pizza.png" />
+body {
+	font-family: Arial;
+	font-size: 17px;
+	padding: 8px;
+}
+
+* {
+	box-sizing: border-box;
+}
+
+.row {
+	display: -ms-flexbox; /* IE10 */
+	display: flex;
+	-ms-flex-wrap: wrap; /* IE10 */
+	flex-wrap: wrap;
+	margin: 0 -16px;
+}
+
+.col-25 {
+	-ms-flex: 25%; /* IE10 */
+	flex: 25%;
+}
+
+.col-50 {
+	-ms-flex: 50%; /* IE10 */
+	flex: 50%;
+}
+
+.col-75 {
+	-ms-flex: 75%; /* IE10 */
+	flex: 75%;
+}
+
+.col-25, .col-50, .col-75 {
+	padding: 0 16px;
+}
+
+.container {
+	background-color: #f2f2f2;
+	padding: 5px 20px 15px 20px;
+	border: 1px solid lightgrey;
+	border-radius: 3px;
+}
+
+input[type=text] {
+	width: 100%;
+	margin-bottom: 20px;
+	padding: 12px;
+	border: 1px solid #ccc;
+	border-radius: 3px;
+}
+
+label {
+	margin-bottom: 10px;
+	display: block;
+}
+
+.icon-container {
+	margin-bottom: 20px;
+	padding: 7px 0;
+	font-size: 24px;
+}
+
+.btn {
+	background-color: #4CAF50;
+	color: white;
+	padding: 12px;
+	margin: 10px 0;
+	border: none;
+	width: 100%;
+	border-radius: 3px;
+	cursor: pointer;
+	font-size: 17px;
+}
+
+.btn:hover {
+	background-color: #45a049;
+}
+
+a {
+	color: #2196F3;
+}
+
+hr {
+	border: 1px solid lightgrey;
+}
+
+span.price {
+	float: right;
+	color: grey;
+}
+
+/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (also change the direction - make the "cart" column go on top) */
+@media ( max-width : 800px) {
+	.row {
+		flex-direction: column-reverse;
+	}
+	.col-25 {
+		margin-bottom: 20px;
+	}
+}
+</style>
+
+
+<body>
+	<div class="row">
+		<div class="col-75">
+			<div class="container">
+				<form method="post">
+
+					<div class="row">
+						<div class="col-50">
+
+							<h3>Change profile information:</h3>
+							<h4>Please insert all fields:</h4>
+
+
+							<label for="fname">First Name</label> <input type="text"
+								id="fname" name="firstname" placeholder="Ivan" required>
+
+							<label for="lname">Last Name</label> <input type="text"
+								id="lname" name="lastname" placeholder="Petrov" required>
+
+							<label for="email">E-mail</label> <input type="text" id="email"
+								name="email" placeholder="ivanpetrov@gmail.com" required>
+
+							<label for="password">Password</label> <input type="password"
+								id="password" name="password" required> <br> <br>
+							<label for="password2">Repeat password</label> <input
+								type="password" id="password2" name="password2" required>
+
+			
+
+						</div>
+					</div>
+
+
+					<input type="submit" value="Change" class="btn">
+
+
+				</form>
+			</div>
+
+
+		</div>
 	</div>
 
-	<h3>Change profile information:</h3>
-	<h4>Please insert all fields:</h4>
-		
-	<h5 style="text-transform:capitalize; text-align: center">
-		
-		<f:form commandName="user">				
-			First name:<f:input path="firstName"/>
-			<f:errors path="firstName" cssClass="error" style = "color:red"></f:errors><br>
-			Last name:<f:input path="lastName"/>
-			<f:errors path="lastName" cssClass="error" style = "color:red"></f:errors><br>
-			E-mail:<f:input path="email"/>
-			<f:errors path="email" cssClass="error" style = "color:red"></f:errors><br>
-			Password:<f:password path="password" placeholder="At least 6 symbols"/>
-			<f:errors path="password" cssClass="error" style = "color:red" ></f:errors><br>
-			
-			<input type="submit" value="Change">			
-		</f:form>
-	</h5>	
+	<script src="js/jquery.min.js"></script>
+	<script src="js/index.js"></script>
 
-</div>
-<script src="js/jquery.min.js"></script>
-<script src="js/index.js"></script>
 </body>
 
 </html>
